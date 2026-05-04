cad1206996f98856fe9212efa995f05f0f48440f
geronimoangelo211-hue <geronimoangelo211@gmail.com>
2026-05-05 00:31:37 +0800

    Create AccountRepository.java

---
+++ src/main/java/com/attendance/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.attendance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<AdminAccount, String> {
+    AdminAccount findByUsername(String username);
+}
